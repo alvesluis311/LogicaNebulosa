f1be9100bebe7484021495adf6d7e94fb2a399e9
alvesluis311 <felipe7311141@gmail.com>
2024-09-12 23:45:25 -0300

    definindo melhor as funções de pertinência renda e divida foram escaladas com incrementos de 1000

--- AtividadeLogicaNebulosa/LogicaNebulosa.py
+++ AtividadeLogicaNebulosa/LogicaNebulosa.py
@@ -6,31 +6,32 @@ class LogicaNebulosa:
     def __init__(self):
         # Variáveis fuzzy de entrada
         self.historico = ctrl.Antecedent(np.arange(0, 11, 1), 'historico')
-        self.renda = ctrl.Antecedent(np.arange(0, 100001, 1), 'renda')
-        self.divida = ctrl.Antecedent(np.arange(0, 100001, 1), 'divida')
+        self.renda = ctrl.Antecedent(np.arange(0, 100001, 1000), 'renda')
+        self.divida = ctrl.Antecedent(np.arange(0, 100001, 1000), 'divida')
         
         # Variável fuzzy de saída
         self.risco = ctrl.Consequent(np.arange(0, 101, 1), 'risco')
         
         # Funções de pertinência para o histórico de crédito
-        self.historico['ruim'] = fuzz.trimf(self.historico.universe, [0, 2, 4])
-        self.historico['regular'] = fuzz.trimf(self.historico.universe, [2, 4, 6])
-        self.historico['bom'] = fuzz.trimf(self.historico.universe, [4, 6, 8])
-        self.historico['excelente'] = fuzz.trimf(self.historico.universe, [6, 8, 10])
-                
+        self.historico['ruim'] = fuzz.trimf(self.historico.universe, [0, 0, 4])
+        self.historico['regular'] = fuzz.trimf(self.historico.universe, [2, 5, 7])
+        self.historico['bom'] = fuzz.trimf(self.historico.universe, [5, 8, 10])
+        self.historico['excelente'] = fuzz.trimf(self.historico.universe, [7, 10, 10])
+        
+        # Funções de pertinência para a renda mensal
         self.renda['baixa'] = fuzz.trimf(self.renda.universe, [0, 0, 20000])
-        self.renda['media'] = fuzz.trimf(self.renda.universe, [0, 50000, 100000])
-        self.renda['alta'] = fuzz.trimf(self.renda.universe, [20000, 100000, 100000])
+        self.renda['media'] = fuzz.trimf(self.renda.universe, [15000, 50000, 85000])
+        self.renda['alta'] = fuzz.trimf(self.renda.universe, [50000, 100000, 100000])
         
         # Funções de pertinência para a dívida atual
         self.divida['baixa'] = fuzz.trimf(self.divida.universe, [0, 0, 20000])
-        self.divida['moderada'] = fuzz.trimf(self.divida.universe, [0, 50000, 100000])
-        self.divida['alta'] = fuzz.trimf(self.divida.universe, [20000, 100000, 100000])
+        self.divida['moderada'] = fuzz.trimf(self.divida.universe, [15000, 50000, 85000])
+        self.divida['alta'] = fuzz.trimf(self.divida.universe, [50000, 100000, 100000])
         
         # Funções de pertinência para o risco
-        self.risco['baixo'] = fuzz.trimf(self.risco.universe, [0, 0, 25])
-        self.risco['medio'] = fuzz.trimf(self.risco.universe, [0, 50, 100])
-        self.risco['alto'] = fuzz.trimf(self.risco.universe, [25, 100, 100])
+        self.risco['baixo'] = fuzz.trimf(self.risco.universe, [0, 0, 33])
+        self.risco['medio'] = fuzz.trimf(self.risco.universe, [25, 50, 75])
+        self.risco['alto'] = fuzz.trimf(self.risco.universe, [66, 100, 100])
         
         self.regras = [
             # Regras com histórico "ruim"
@@ -54,8 +55,6 @@ class LogicaNebulosa:
             ctrl.Rule(self.historico['excelente'] & self.divida['baixa'] & self.renda['alta'], self.risco['baixo']),
         ]
 
-       
-        
         # Controle fuzzy
         self.sistema_controle = ctrl.ControlSystem(self.regras)
         self.simulador = ctrl.ControlSystemSimulation(self.sistema_controle)
